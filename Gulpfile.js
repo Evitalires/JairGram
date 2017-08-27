420401aeff2a0f0887f853d1a902261fba23d0fd
Jair Escobar <evitalires@gmail.com>
2017-08-27 17:43:09 -0500

    Organizando assets, e intento de incrustar favicon en la web

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var rename = require('gulp-rename')
-//Vamos a definir una tarea para gulp
-//(nombre tarea, función) => Parsear sass
-gulp.task('styles', function (){
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+gulp.task('styles', function () {
   gulp
     .src('index.scss')
     .pipe(sass())
     .pipe(rename('app.css'))
     .pipe(gulp.dest('public'));
 })
-gulp.task('assets', function(){
+
+gulp.task('assets', function () {
   gulp
     .src('assets/*')
     .pipe(gulp.dest('public'));
 })
-//Definiendo la tarea por defecto
+
 gulp.task('default', ['styles', 'assets'])
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+
+var app = express();
 
 app.set('view engine', 'pug');
 
-app.use(express.static('public'))
-//      Ruta, función
-app.get('/', function(req, res){
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
   res.render('index');
 })
 
-// Ahora vamos a lanzar el sitio web
-// Puerto que escuchará,                      Cerramos el proceso con 1
-app.listen(3000, function(err){
-  if (err) return console.log('Hubo un error'), process.exit(1)
-  console.log('JairGram escuchando en el puerto 3000')
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Platzigram escuchando en el puerto 3000');
 })
