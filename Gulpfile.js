b2db632e3bc14ac35a8a79919f32da48d2243dbd
Jair Escobar <evitalires@gmail.com>
2017-08-26 22:09:02 -0500

    Instalación paquetes(gulp, sass, materialize)| Creación archivos (gulpfile,index.scss)

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var rename =require('gulp-rename')
+//Vamos a definir una tarea para gulp
+//(nombre tarea, función) => Parsear sass
+gulp.task('styles', function (){
+  gulp
+    .src('index.scss')
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public'));
+})
+//Definiendo la tarea por defecto
+gulp.task('default', ['styles'])
