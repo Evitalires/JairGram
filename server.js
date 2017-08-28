c32a497da16936033ee4046bf6bd0e7d560ac84e
Jair Escobar <evitalires@gmail.com>
2017-08-27 22:39:02 -0500

    Instalación: browserify, vinyl-source, babelify || Creación: app.js e integración en index.pug como script

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get('/', function (req, res) {
 app.listen(3000, function (err) {
   if (err) return console.log('Hubo un error'), process.exit(1);
 
-  console.log('Platzigram escuchando en el puerto 3000');
+  console.log('JairGram escuchando en el puerto 3000');
 })
