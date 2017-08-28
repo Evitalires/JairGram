08ba2994ee03145d8ee16634308537c03e2cfb08
Jair Escobar <evitalires@gmail.com>
2017-08-28 02:03:15 -0500

    Instalación de watchify || automatizando el bundle en gulpfile.js y añadiendo scripts en el package.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('Hola');
+const page = require('page')
+
+const main = document.getElementById('main-container')
+
+page('/', function (contex, next) {
+  main.innerHTML = 'Home <a href="/signup"> Signup </a>'
+})
+page('/signup', function (contex, next) {
+  main.innerHTML = 'Signup <a href="/"> Home </a>'
+})
+page('/signin', function (contex, next) {
+  main.innerHTML = 'Signin <a href="/signup"> Signup </a>' 
+})
+page()
