8cf0433c6b721f4cd3851d95ee23cc5ff4fd1deb
Jair Escobar <evitalires@gmail.com>
2017-08-30 21:11:32 -0500

    Instalación de moment.js para establecer las fechas relativas||También configuramos las fechas

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -15,7 +15,8 @@ page('/', function (ctx, next) {
       },
       url: 'https://carlosazaustre.es/content/images/2015/02/B9p1VCGIEAAZcUJ.jpg',
       likes: 10,
-      liked: true
+      liked: true,
+      createdAt: new Date()
     },
     {
       user: {
@@ -24,7 +25,8 @@ page('/', function (ctx, next) {
       },
       url: 'https://userscontent2.emaze.com/images/e0db49a7-545f-427e-bb4e-95f990e2234b/5d80eb38c5581aafcdc06d0b3c1cc6e8.jpg',
       likes: 1,
-      liked: true
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 100)
     }
   ];
   empty(main).appendChild(template(pictures));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const page = require('page');
+var moment = require('moment');
+
+require('moment/locale/es');
+moment.locale('es');
 
 require('./homepage');
 require('./signup');
